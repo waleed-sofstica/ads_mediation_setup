06eb624ff04acfb35b94b3cb5152fc4c2868035c
Syed Waleed Shah <87638939+waleed-sofstica@users.noreply.github.com>
2021-12-13 12:06:54 +0500

    added android and ios folders for testing the plugin

--- android/app/src/main/kotlin/com/example/mediation_plugin_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mediation_plugin_test/MainActivity.kt
@@ -2,5 +2,7 @@ package com.example.mediation_plugin_test
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity()
+{
+
 }

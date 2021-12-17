7ba99e201800f8271d9d6e2135db6724c36e75b8
Syed Waleed Shah <87638939+waleed-sofstica@users.noreply.github.com>
2021-12-17 17:47:00 +0500

    updated google class

--- lib/models/google.dart
+++ lib/models/google.dart
@@ -1,37 +1,57 @@
 class Google {
-  late String appId;
-  late String banner;
-  late String adManagerBanner;
-
-  late String interstitial;
-  late String rewarded;
+  late String appIdIOS;
+  late String bannerIOS;
+  late String adManagerBannerIOS;
+  late String interstitialIOS;
+  late String rewardedIOS;
+  late String appIdAndroid;
+  late String bannerAndroid;
+  late String adManagerBannerAndroid;
+  late String interstitialAndroid;
+  late String rewardedAndroid;
   final String sdk = 'com.google.android.gms:play-services-ads:';
   late String sdkVersion;
 
-  Google(
-      {required this.appId,
-      required this.sdkVersion,
-      required this.banner,
-      required this.adManagerBanner,
-      required this.interstitial,
-      required this.rewarded});
+  Google({
+    required this.appIdIOS,
+    required this.sdkVersion,
+    required this.bannerIOS,
+    required this.adManagerBannerIOS,
+    required this.interstitialIOS,
+    required this.rewardedIOS,
+    required this.appIdAndroid,
+    required this.bannerAndroid,
+    required this.adManagerBannerAndroid,
+    required this.interstitialAndroid,
+    required this.rewardedAndroid,
+  });
 
   Google.fromJson(Map<String, dynamic> json) {
-    appId = json['appId'];
-    banner = json['banner'];
-    adManagerBanner = json['adManagerBanner'];
-    interstitial = json['interstitial'];
-    rewarded = json['rewarded'];
+    appIdIOS = json['appIdIOS'];
+    bannerIOS = json['bannerIOS'];
+    adManagerBannerIOS = json['adManagerBannerIOS'];
+    interstitialIOS = json['interstitialIOS'];
+    rewardedIOS = json['rewardedIOS'];
+    appIdAndroid = json['appIdAndroid'];
+    bannerAndroid = json['bannerAndroid'];
+    adManagerBannerAndroid = json['adManagerBannerAndroid'];
+    interstitialAndroid = json['interstitialAndroid'];
+    rewardedAndroid = json['rewardedAndroid'];
     sdkVersion = json['sdkVersion'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['appId'] = this.appId;
-    data['banner'] = this.banner;
-    data['adManagerBanner'] = this.adManagerBanner;
-    data['interstitial'] = this.interstitial;
-    data['rewarded'] = this.rewarded;
+    data['appIdIOS'] = this.appIdIOS;
+    data['bannerIOS'] = this.bannerIOS;
+    data['adManagerBannerIOS'] = this.adManagerBannerIOS;
+    data['interstitialIOS'] = this.interstitialIOS;
+    data['rewardedIOS'] = this.rewardedIOS;
+    data['appIdAndroid'] = this.appIdAndroid;
+    data['bannerAndroid'] = this.bannerAndroid;
+    data['adManagerBannerAndroid'] = this.adManagerBannerAndroid;
+    data['interstitialAndroid'] = this.interstitialAndroid;
+    data['rewardedAndroid'] = this.rewardedAndroid;
     data['sdkVersion'] = this.sdkVersion;
     return data;
   }

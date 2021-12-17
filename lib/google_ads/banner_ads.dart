d0554708c077bb900a8ca3bafa62ac2b47b1c31a
Syed Waleed Shah <87638939+waleed-sofstica@users.noreply.github.com>
2021-12-17 12:25:59 +0500

    Added flutter classes to show banner,admanagerbanner,interstitial and rewarded ads

---
+++ lib/google_ads/banner_ads.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:ads_mediation_setup/google_ads/ad_unit_id.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class BannerAdsProvider {
+  BannerAd? _bannerAd;
+  bool _bannerAdIsLoaded = false;
+  String bannerAdId;
+
+  get available {
+    return _bannerAdIsLoaded == true && _bannerAd != null;
+  }
+
+  BannerAd? get ad => _bannerAd;
+
+  BannerAdsProvider({required this.bannerAdId}) {
+    _initialize();
+  }
+
+  void _initialize() {
+    _bannerAd = BannerAd(
+        size: AdSize.banner,
+        adUnitId: bannerAdId.isEmpty ? BannerAd.testAdUnitId : bannerAdId,
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) {
+            print('$BannerAd loaded.');
+            _bannerAdIsLoaded = true;
+          },
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            print('$BannerAd failedToLoad: $error');
+            ad.dispose();
+          },
+          onAdOpened: (Ad ad) => print('$BannerAd onAdOpened.'),
+          onAdClosed: (Ad ad) => print('$BannerAd onAdClosed.'),
+        ),
+        request: AdRequest())
+      ..load();
+  }
+
+  void dispose() {
+    _bannerAd?.dispose();
+  }
+}
